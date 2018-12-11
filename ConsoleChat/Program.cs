c2e46aa2057513164043edc522a4d0cb1b39e73c
Lviv\Volodymyr_Mykytiuk <Volodymyr_Mykytiuk@epam.com>
2018-12-11 17:53:16 +0200

    Console chat created

---
+++ ConsoleChat/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using HelperBot;
+using Models.Bot;
+using Telegram.Bot.Args;
+
+namespace ConsoleChat
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var chat = Chat.Instance;
+      chat.SubscribeToMessage(ChatMessageReceiver);
+
+      Console.WriteLine("Bot is ready!");
+
+      while (true)
+      {
+        var input = Console.ReadLine();
+        if (input?.Equals("Exit", StringComparison.OrdinalIgnoreCase) ?? false)
+        {
+          break;
+        }
+
+        chat.SendMessage(input);
+      }
+
+    }
+
+    public static void ChatMessageReceiver(object sender, MessageEventArgs args)
+    {
+      Console.WriteLine($"{args.Message.From}: {args.Message.Text}");
+    }
+  }
+}
---
+++ HelperBot/Chat.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using DAL.Data;
+using Models.Bot;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Utils;
+
+namespace HelperBot
+{
+  public class Chat : IDisposable
+  {
+    public static Chat Instance
+    {
+      get {
+        if (_instance == null)
+        {
+          _instance = new Chat();
+        }
+
+        return _instance;
+      }
+    }
+
+    private Chat()
+    {
+      _storage = new FileStorage();
+      _bot = new TelegramBotClient(ConfigManager.Settings.AppConfig.BotId);
+
+      _bot.OnMessage += DefaultHandler;
+      _bot.OnMessageEdited += DefaultHandler;
+
+      _bot.StartReceiving();
+    }
+
+    public void SendMessage(String input)
+    {
+      _storage.ReadAll<Client>().ToList().ForEach(client =>
+        _bot.SendTextMessageAsync(client.ChatId, input)
+      );
+    }
+
+    public void SubscribeToMessage(System.EventHandler<MessageEventArgs> handler)
+    {
+      _bot.OnMessage += handler;
+      _bot.OnMessageEdited += handler;
+    }
+
+    public void DefaultHandler(object sender, MessageEventArgs args)
+    {
+      _storage.Write(new Client { ChatId = args.Message.Chat.Id });
+    }
+
+    public void Dispose()
+    {
+      _bot.StopReceiving();
+    }
+
+    private static Chat _instance;
+    private readonly TelegramBotClient _bot;
+    private readonly FileStorage _storage;
+  }
+}
